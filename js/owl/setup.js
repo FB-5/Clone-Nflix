4592107fe70a478727897cfa9a35bbf6f8bd212f
Filipe Bayao <81266323+FB-5@users.noreply.github.com>
2021-07-21 21:05:35 -0300

    Add files via upload

---
+++ js/owl/setup.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+. cookie='cookie1 = valor1; SameSite = Lax ';
+
+. cookie  =  'cookie2 = valor2; SameSite = Nenhum; Seguro ' ;
